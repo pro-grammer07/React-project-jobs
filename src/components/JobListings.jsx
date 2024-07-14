beda1d5acc6c4bc9812e4dc41f7af0cdd4575380
Syeda Ayesha <syedaayeshawajahat@gmail.com>
2024-07-14 15:03:45 +0500

    proxy for path

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -11,7 +11,7 @@ const JobListings = ({ isHome = false }) => {
   useEffect(() => {
     const fetchJobs = async () => {
       console.log(isHome);
-      const apiUrl = isHome ? "http://localhost:8000/jobs?_limit=3" : "http://localhost:8000/jobs";
+      const apiUrl = isHome ? "/api/jobs?_limit=3" : "/api/jobs";
       try {
         const res = await fetch(apiUrl);
         const data = await res.json();
